06c68b333c64357f62ed2f273f671ea6bd5eb9af
isil <isilozgu2016@gmail.com>
2025-04-30 16:04:05 +0300

    deleted params to try running the code on ci 2

--- RepositorySearch/RetrieveLanguages.py
+++ RepositorySearch/RetrieveLanguages.py
@@ -6,23 +6,7 @@ from RepositorySearch.utils import write_json_file
 
 
 def get_repository_with_language():
-    """
-        Retrieves repositories with a specific language from GitHub.
 
-        Parameters
-        ----------
-        None
-
-        Returns
-        -------
-        list
-            A list of dictionaries where each dictionary contains the name of the repository and its languages.
-
-        Notes
-        -----
-        The function currently searches for repositories written in Java, sorted by stars in descending order,
-        and retrieves a maximum of 10 repositories. The parameters for the search query can be modified within the function.
-    """
 
     params = {
     }
